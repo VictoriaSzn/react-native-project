d8f9b97937406f89f63af6956a9e340de65a0c00
VictoriaSzn <victoriia.szn@gmail.com>
2023-05-12 21:25:05 +0300

    2.1

--- App.js
+++ App.js
@@ -1,172 +1,113 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import {
-  Button, ImageBackground, StyleSheet, Text, TextInput, View, TouchableOpacity,
+import { useCallback, useState } from 'react';
+import {ImageBackground, StyleSheet, Text, TextInput, View, TouchableOpacity,
   Platform, KeyboardAvoidingView, Keyboard, TouchableWithoutFeedback
 } from 'react-native';
-import { useState } from 'react';
+
 const initialState = {
   login: '',
   email: '',
   password:''
 }
-// const loadAplication = async () => {
-//   await Font.loadAsync({
-//     'Roboto-Medium':require('./assets/fonts/Roboto-Medium.ttf'),
-//    });
-// };
-
-SplashScreen.preventAutoHideAsync();
-
 export default function App() {
- const [fontsLoaded] = useFonts({
-    'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'),
-    'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
- });
-  // const onLayoutRootView = useCallback(async () => {
-  //   if (fontsLoaded) {
-  //     await SplashScreen.hideAsync();
-  //   }
-  // }, [fontsLoaded]);
-
-  if (!fontsLoaded) {
-    return null; 
-}
-   const [isShowKeyboard, setIsShowKeyboard] = useState(false);
-   const [state, setState] = useState(initialState);
+  //console.log(Platform.OS);
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  const [state, setState] = useState(initialState);
    const KeyboardHide = () => {
-     setIsShowKeyboard(false);/////
+     setIsShowKeyboard(false);
      Keyboard.dismiss();
-     //setState(initialState);
+     setState(initialState);
+     //console.log(state);
    }
-   return (
+  
+  return (
+    <TouchableWithoutFeedback onPress={KeyboardHide}>
     <View style={styles.container}>
-       <TouchableWithoutFeedback
-         //</View>
-         onPress={KeyboardHide}
-       >
       <ImageBackground
-        source={require("./assets/images/bgimage.jpg")}
-        style={{ ...styles.image, marginBottom: isShowKeyboard ? 0 : 0}}
-        //style={styles.image}
-      >
-{/* FORM */}
-        <KeyboardAvoidingView
-          behavior={Platform.OS == 'ios' ? 'padding' : 'height'}>
-          
-          <View
-            //style={styles.form}
-            style={{ ...styles.form, marginBottom: isShowKeyboard ? 20 : 100 }}
-          >
-            
-           {/* <View style={styles.bordertext}></View> */}
-           <Text style={styles.text} >Реєстрація</Text>
-                  
-{/* LOGIN */}
-          <View
-            // style={{ marginTop: 20 }}
-            >
-            {/* <Text style={styles.label}>LOGIN</Text>   */}
-            <TextInput
-              // всередині записують пропси
+         style={styles.image} 
+         source={require("./assets/images/bgimage.jpg")}
+       >
+         <KeyboardAvoidingView
+          //behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
+         >
+           <View style={{
+             ...styles.form,
+             marginBottom: isShowKeyboard ? 10 : 0
+           }}>
+         <Text style={styles.titleForm} >Реєстрація</Text>
+          <TextInput
             placeholder="Логін"      
-            style={styles.input}
-            onFocus={() => setIsShowKeyboard(true)}
+             style={{ ...styles.input, marginBottom: 16 }}
+           onFocus={() => setIsShowKeyboard(true)}
             value ={state.login}
             onChangeText={(value)=>setState((prevState)=>({...prevState, login:value}))}              
-              //textAlign={"center"}  
-            // keyboardType="numeric"
-            />
-          </View>
- {/* MAIL */}
-          <View>
-            {/* <Text style={styles.label}>EMAIL ADDRES</Text> */}
-            <TextInput
-              // всередині записують пропси
-              placeholder="Електронна пошта"    
-              style={styles.input}
+          />
+          <TextInput
+             placeholder="Електронна пошта"    
+             style={{ ...styles.input, marginBottom: 16 }}
               onFocus={() => setIsShowKeyboard(true)}
               value ={state.email}
               onChangeText={(value)=>setState((prevState)=>({...prevState, email:value}))}
-              //textAlign={"center"}              
-            />
-          </View>
- {/* password */}
-          <View
-            // style={{ marginTop: 20 }}
-          >
-            {/* <Text style={styles.label}>PASSWORD</Text>   */}
-            <TextInput
-              // всередині записують пропси
-              style={styles.input}
+          />
+         <TextInput
+             style={{ ...styles.input, marginBottom: 43 }}
               secureTextEntry={true}
               placeholder="Пароль"
               onFocus={() => setIsShowKeyboard(true)}
               value={state.password}
               onChangeText={(value)=>setState((prevState)=>({...prevState, password:value}))}
-            />
-          </View>
-            <TouchableOpacity
+              />
+              {/* показати на паролі
+           <TouchableOpacity style={styles.btn}><Text style={styles.btnTitle}>Показати</Text></TouchableOpacity> */}
+           <TouchableOpacity
               style={styles.btn}
               activeOpacity={0.8}
-              onPress={KeyboardHide}>
-            <Text style={styles.btnTitle}>Зареєструватись</Text>
-            </TouchableOpacity>
-            
-          <Text style={styles.textDn}>Вже маєте акаунт? Увійти.</Text>
-        </View>
-        </KeyboardAvoidingView>
+             onPress={KeyboardHide}
+           >
+             <Text style={styles.btnTitle}>Зареєструватись</Text>
+           </TouchableOpacity>
+            <Text style={styles.textDn}>Вже маєте акаунт? Увійти.</Text>
+           </View>
+           </KeyboardAvoidingView>
       </ImageBackground>
-      </TouchableWithoutFeedback>
-       <StatusBar style="auto" />
-    </View>
+     </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    //alignItems: 'center',
-   // justifyContent: 'center',
-    
+    backgroundColor: '#808080',
   },
-  //все лижить в контейнері image і центрується як в ньому задано
-   image: {
+  image: {
     flex: 1,
      resizeMode: "cover",
      //justifyContent: 'center',//////
-     justifyContent: 'flex-end',
-   //alignItems: 'center',
-   //justifyContent: 'space-around'////
-  },
-  form: {
-    //marginHorizontal: 10,
-     backgroundColor: '#ffffff',
-     borderColor: "black",
-    borderRadius: 10,
-    paddingBottom:78,
-     //marginBottom:0,
+     //justifyContent: 'flex-end',
+    //justifyContent: 'space-around'////
     
   },
-  //label:{
-  //marginBottom: 10,
-  //},
-
-    text: {
+   form: {
+    backgroundColor: '#ffffff',
+    borderColor: "black",
+    borderRadius: 10,
+    paddingTop:92,
+    paddingBottom:78,
+    paddingHorizontal: 16,
+  },
+    titleForm: {
     fontWeight: 500,
     textAlign: 'center', 
-      fontSize: 30,
-    fontFamily: 'Roboto-Medium',
+    fontSize: 30,
+    //fontFamily: 'Roboto-Medium',
     color: '#212121',
-    marginBottom: 32,  
-    
+    marginBottom: 32
   },
-  input: {
+    input: {
     height: 50,
-    margin: 16,
-    marginTop: 0,
     padding: 10,
     borderWidth: 1,
     borderRadius: 8,
@@ -174,36 +115,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#f6f6f6',
     fontSize:16,
   },
-
-    bordertext:{
-     borderWidth: 2,
-     borderColor: "black",
-     borderRadius: 16,
-      padding: 40,
-      width: 120,
-      height: 120,
-     alignItems: 'center'
-   },
-  btn:{
+    btn:{
     backgroundColor: '#ff6c00', 
     height: 50,
     borderRadius: 100,
-    margin: 16,
-    marginTop:43, ////////перекриття
     justifyContent: "center",
-    alignItems: "center",
-    
+      alignItems: "center",
+    marginBottom:16,
   },
-  btnTitle: {
+    btnTitle: {
     color: "#ffffff",
     fontSize:16,
   },
-   textDn: {
+     textDn: {
      fontWeight: 400,
      fontSize: 16,
      justifyContent:'center',
      color: '#1b4371',
      textAlign: 'center', //////?????
-    
   },
 });
