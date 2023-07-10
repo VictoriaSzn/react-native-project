b5ec3e626e5ec46eafccd7b53d0958d6a18c7e70
VictoriaSzn <victoriia.szn@gmail.com>
2023-07-10 20:08:35 +0300

    5-5

--- screens/mainScreen/createScreen.js
+++ screens/mainScreen/createScreen.js
@@ -1,16 +1,54 @@
-import React from 'react';
-import {View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {View, Text, Image, StyleSheet } from 'react-native';
 import { Camera } from 'expo-camera';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { TouchableOpacity } from 'react-native';
+import * as Location from 'expo-location';
 
-const CreateScreen = () => {
+const CreateScreen = ({navigation}) => {
+    const [camera, setCamera] = useState(null);
+    const [photo, setPhoto] = useState(null);
+
+    const takePhoto = async () => {
+        const photo = await camera.takePictureAsync();
+        const location = await Location.getCurrentPositionAsync();
+        setPhoto(photo.uri);
+     // console.log("location", location);
+   };
+    
+    const sendPhoto = async () => {
+        navigation.navigate("Posts", {photo});
+    }
+    useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+    })();
+  }, []);
     return (
         <View style={styles.container}>
-            <Camera style={styles.camera}>
-                <TouchableOpacity style={styles.snapContainer} onPress={() => { }}>
-                    <Text style={styles.snap}>SNAP</Text>
+            <Camera style={styles.camera} ref={setCamera}>
+                 {photo && ( 
+                    <View style={styles.photoContainer}>
+                        <Image
+                            source={{ uri: photo }}
+                            style={{ width: 200, height: 200 }}
+                        />
+                    </View>
+                )} 
+                <TouchableOpacity style={styles.snapContainer} onPress={takePhoto} >
+                    <Text style={styles.snapText}>SNAP</Text>
                 </TouchableOpacity>
             </Camera>
+            <View>
+                 <TouchableOpacity style={styles.sendContainer} onPress={sendPhoto} >
+                    <Text style={styles.sendLabel}>Send</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 };
@@ -19,22 +57,50 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     camera: {
-        height: 300,
-        marginTop: 50,
+        //flex:1,
+        height: "70%",
+        marginHorizontal: 2,
+        borderRadius: 10,
+        marginTop: 40,
         alignItems: "center",
+        justifyContent: "flex-end",
     },
     snapContainer: {
-        marginTop: 200,
         borderWidth: 1,
-        borderColor: "#ff0000",
+        borderColor: "#fff",
         width: 70,
         height: 70,
         borderRadius: 50,
         justifyContent: "center",
-        alignItems:"center",
+        alignItems: "center",
+         marginBottom: 20,
     },
-    snap: {
+    snapText: {
         color: "#fff",
+    },
+    photoContainer: {
+        position: "absolute",
+        top: 50,
+        left: 10,
+        borderWidth: 1,
+        borderColor: "#fff",
+        borderRadius: 10,       
+        
+    },
+    sendContainer: {
+        marginHorizontal: 30,
+        height: 40,
+        borderWidth: 2,
+        borderColor: "#000",
+        //width: 70,
+        borderRadius: 10,
+        justifyContent: "center",
+        alignItems: "center",
+         marginTop: 20,
+    },
+    sendLabel: {
+        color: "#000",
+        fontSize: 20,
     }
 
 });
--- screens/mainScreen/postsScreen.js
+++ screens/mainScreen/postsScreen.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import {View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {View, Text, StyleSheet, FlatList, Image } from 'react-native';
 
-const PostsScreen = () => {
+const PostsScreen = ({ route }) => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        if (route.params) {
+            setPosts(prevState => [...prevState, route.params]);
+        }
+    }, [route.params]);
+
     return(
-    <View style={styles.container}>
-        <Text>PostsScreen</Text>
+    <View style={styles.container}> 
+            <FlatList
+                data={posts}
+                keyExtractor={(item, indx) => indx.toString()}
+                renderItem={({ item }) => (
+                    <View style={{marginBottom: 10, justifyContent: "center", alignItems: "center"}}>
+                        <Image source={{ uri: item.photo }}
+                            style={{ width: 350, height: 200 }} />
+                    </View>
+                )}
+            />
     </View >
     );
 };
@@ -12,7 +29,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: "center",
-        alignItems: "center",
+        //alignItems: "center",
+        paddingTop:40,
     },
 });
 
